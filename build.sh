b497f6fe3632099ef7f915594d04fa1f7cdd0c99
LuanHalaiko <tecnotailsplays@gmail.com>
2018-03-08 21:57:51 -0300

    build: Back to Uber!

--- build.sh
+++ build.sh
@@ -49,12 +49,12 @@ MIUI_ZIP_DIR=$KERNEL_DIR/miui_repack
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
 
 #export
-export CROSS_COMPILE="$HOME/kernel/linaro/bin/aarch64-linux-gnu-"
+export CROSS_COMPILE="$HOME/kernel/Uber-8.0/bin/aarch64-linux-android-"
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="LuanHalaiko"
-export KBUILD_BUILD_HOST="LH-Compilator"
-export KBUILD_LOUP_CFLAGS=
+export KBUILD_BUILD_HOST="CrossBuilder"
+export KBUILD_LOUP_CFLAGS="-Wno-misleading-indentation -Wno-bool-compare -O2"
 
 #misc
 CONFIG=santoni_defconfig
