89043c1b5d58d25b40d10402e4b856fac7542251
Ryan Andri <ryanandri@linuxmail.org>
2018-03-18 23:26:11 +0700

    usb: phy-msm-usb: Xiaomi kernel changes for Redmi 4/4X
    conflicts:
    	drivers/usb/phy/phy-msm-usb.c
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -123,6 +123,7 @@ enum msm_usb_phy_type {
 #define IDEV_CHG_MAX	1500
 #define IUNIT		100
 #define IDEV_HVDCP_CHG_MAX	1800
+#define IDEV_CHG_MIN 500
 
 /**
  * Different states involved in USB charger detection.
@@ -428,6 +429,8 @@ struct msm_otg {
 	struct clk *phy_csr_clk;
 	struct clk *bus_clks[USB_NUM_BUS_CLOCKS];
 	struct clk *phy_ref_clk;
+	spinlock_t otg_phy_clk_lock;
+	bool otg_phy_clk_enable;
 	long core_clk_rate;
 	long core_clk_svs_rate;
 	long core_clk_nominal_rate;
