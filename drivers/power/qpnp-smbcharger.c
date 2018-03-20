3549a2397569468556b82b2b45d14abfa36848f5
Ryan Andri <ryanandri@linuxmail.org>
2018-03-20 23:36:53 +0700

    power: qpnp-smbcharger: set back to 2000ma max allowed.
    this commits purpose to Quick charger 2.0.
    also change value for Quick charger 3.0 to allowed
    max miliampere upto 2400ma, 2000ma is safe for normal
    charger too.
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -451,19 +451,19 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_hvdcp_icl_ma = 2800;
+static int smbchg_default_hvdcp_icl_ma = 2000;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_hvdcp3_icl_ma = 3000;
+static int smbchg_default_hvdcp3_icl_ma = 2400;
 module_param_named(
 	default_hvdcp3_icl_ma, smbchg_default_hvdcp3_icl_ma,
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 2500;
+static int smbchg_default_dcp_icl_ma = 2000;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
