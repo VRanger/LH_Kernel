c72c02a2ff378ed44ed4ddfca0efe127621e3b09
LuanHalaiko <tecnotailsplays@gmail.com>
2018-06-17 02:02:54 -0300

    scripts: add copyright reference -Add @LuizPauloDamaceno github email for copyright reference

--- r.sh
+++ r.sh
@@ -2,7 +2,7 @@
 #
 # Revert script
 #
-# Copyright (C) 2018 Luan Halaiko and LuizPauloDamaceno (tecnotailsplays@gmail.com)
+# Copyright (C) 2018 Luan Halaiko and LuizPauloDamaceno (tecnotailsplays@gmail.com) (luizpaulodamaceno@live.com)
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
