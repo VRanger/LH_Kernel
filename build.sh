a8ec13c6083d8467ddb5b91d10572981c7770b61
Luan Halaiko <luanhalaiko@github.com>
2018-07-16 23:44:46 -0300

    LH: Back to UBER-8 :tada:

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ ZIP_DIR=$KERNEL_DIR/Zipper
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
 
 #Export
-export CROSS_COMPILE="$HOME/kernel/UBER-7/bin/aarch64-linux-android-"
+export CROSS_COMPILE="$HOME/kernel/UBER-8/bin/aarch64-linux-android-"
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="LuanHalaiko"
